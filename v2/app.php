7bb06fd1017a86d206f7e5793ba5c75bcebc123a
Hamza <ismnoiet@users.noreply.github.com>
2015-08-05 15:44:00 +0100

    remove unneeded content

--- v2/app.php
+++ v2/app.php
@@ -2,34 +2,20 @@
 	
 	// 1 : load the table module 	
 	require_once('modules/table.php');
+	
 	// 2 : create an instnce of the Table module
 	$table = new Table();
 
 	// 3 : configuring the new create table 
-	// $table->name('table1');
-	// $table->add()->name('id')->type('int')->size('10')->close()
-	//       ->add()->name('name')->type('varchar')->size('100')->close();
-
-
-
-	 $table->name('user');
-	 $table->add()->name('id')->type('int')->size('3')->close();
-	 $table->add()->name('name')->type('varchar')->size('50')->close();
-	 
-
-	 
-
-	// echo $table->toString();      
-
-	// echo $table->create();
-	// print_r($table->read(3));
-	// echo $table->update();
-	// echo $table->delete();
-	
-	// $a = new table1();
-	// print_r($a->read(3));
-	
-	// 4 : exporting the newly created table class	 
+		
+		// 3.1 give the table a name 
+		 $table->name('user');
+		 // 3.2 add the id column
+		 $table->add()->name('id')->type('int')->size('3')->close();
+		 // 3.3 add the name column 
+		 $table->add()->name('name')->type('varchar')->size('50')->close();
+		 
+	// 4 : export the newly created table class to "tables" directory	 
 	echo $table->export();	 
 
 ?>
